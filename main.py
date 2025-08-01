cddb416f19922a2ead3ef2813b1653c403a062e6
livius09 <levi.wimroither@hotmail.com>
2025-08-01 19:38:58 +0200

    started whit some definitions

---
+++ main.py
@@ -0,0 +1,14 @@
+ownfiles=[]
+size_left_kb=0
+
+class N_file():
+    def __init__(self) -> None:
+        self.name=""
+        self.spread=1
+        self.prio=3
+        self.size_kb=0
+        
+    def send_self(self):
+        pass
+
+
